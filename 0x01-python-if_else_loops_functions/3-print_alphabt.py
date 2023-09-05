e449e4df649a9bd1584daebb8d646320d213a417
faith221 <edithbandao14@gmail.com>
2023-09-05 11:25:33 -0700

    Corrections

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 for lowercase_letter in range(97, 123):
-    if chr(lowercase_letter) !=  "q" and chr(lowercase_letter) != "e":
+    if chr(lowercase_letter) != "q" and chr(lowercase_letter) != "e":
         print("{}".format(chr(lowercase_letter)), end="")
