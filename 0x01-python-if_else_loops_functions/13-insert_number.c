b47b5e976718d717ae9a244d984e1af7e98ab19a
faith221 <edithbandao14@gmail.com>
2023-09-05 12:05:49 -0700

    Python - if/else, loops, functions

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -2,28 +2,28 @@
 /**
  * insert_node - It inserts a num into a sorted singly-linked list.
  * @head: A pointr to the head of the linked list.
- * @num: The number to insert.
+ * @number: The number to insert.
  * Return: NULL if function fails.
  * Otherwise the address of the new node.
  */
 
-listint_t *insert_node(listint_t **head, int num)
+listint_t *insert_node(listint_t **head, int number)
 {
-	listint *node = *head, *new_nd;
+	listint_t *node = *head, *new_node;
 
 	new_node = malloc(sizeof(listint_t));
 	if (new_node == NULL)
 		return (NULL);
-	new_node->n = num;
+	new_node->n = number;
 
-	if (node == NULL || node->n >= num)
+	if (node == NULL || node->n >= number)
 	{
 		new_node->next = node;
-		*head = new_node
+		*head = new_node;
 			return (new_node);
 	}
 
-	while (node && node->next && node->next->n < num)
+	while (node && node->next && node->next->n < number)
 		node = node->next;
 	node->next = new_node;
 
--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -2,4 +2,3 @@
 for digit1 in range(10):
     for digit2 in range(digit1 + 1, 10):
         print("{}{},".format(digit1, digit2), end=" ")
-
