bafa7d40ff7056d3808bd8697370c43aae125938
faith221 <edithbandao14@gmail.com>
2023-09-06 04:51:14 -0700

    edited

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
-output = ""
 for digit1 in range(10):
     for digit2 in range(digit1 + 1, 10):
-        if digit2 < 9:
-            output += "{}{},".format(digit1, digit2)
-        else:
-            output += "{}{}".format(digit1, digit2)
-print(output.rstrip(","))
+       if digit1 < 9 or digit2 < 9:
+           print("{}{},".format(digit1, digit2), end=" ")
+       else:
+           print"{}{}".format(digit1, digit2)
