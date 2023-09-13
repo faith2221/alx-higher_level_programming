e847ef313d1dae9b0cdf61d437f41382c8ac38f5
faith221 <edithbandao14@gmail.com>
2023-09-13 06:01:26 -0700

    Python - More Data Structures: Set, Dictionary

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def weight_average(my_list=[]):
+    if not my_list:
+        return 0
+    total = 0
+    res = 0
+    
+    for score, weight in my_list:
+        total += score * weight
+        res += weight
+    if res == 0:
+        return 0
+    return total / res
+
