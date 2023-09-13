74c02981149c4e13ef423c9276d260e39e3ece5d
faith221 <edithbandao14@gmail.com>
2023-09-13 06:50:30 -0700

    Deletetion of whitespace

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -3,7 +3,7 @@ def weight_average(my_list=[]):
     if not my_list:
         return 0
     total = 0
-    res = 0 
+    res = 0
     for score, weight in my_list:
         total += score * weight
         res += weight
