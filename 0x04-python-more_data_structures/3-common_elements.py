b2beb2ed6b195c528b4dcb6408fd1da30e6cfa62
faith221 <edithbandao14@gmail.com>
2023-09-13 02:03:59 -0700

    Python - More Data Structures: Set, Dictionary

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def common_elements(set_1, set_2):
-    com_set = set_1.intersection(set_2)
-    return com_set
+    res = set_1.intersection(set_2)
+    return res
---
+++ 0x04-python-more_data_structures/5-number_keys.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+def number_keys(a_dictionary):
+    res = list(a_dictionary.keys())
+    return len(res)
