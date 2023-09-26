6db5905d1a4d6fa2da684c3f6fedf2c2337c842a
faith221 <edithbandao14@gmail.com>
2023-09-26 10:15:54 -0700

    New additions to 5-square.py

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -3,6 +3,7 @@
 
 
 class Square:
+    """Represents a square."""
     def __init__(self, size=0):
         """Initializes a new Square Instance."""
 
@@ -11,13 +12,11 @@ class Square:
     @property
     def size(self):
         """Retrieves and returns the size of the square."""
-        
         return self.__size
 
     @size.setter
     def size(self, value):
         """Sets the size of the square."""
-
         if not isinstance(value, int):
             raise TypeError("size must be an integer")
         if value < 0:
@@ -26,12 +25,10 @@ class Square:
 
     def area(self):
         """Computes and returns the area of the square."""
-        
         return self.__size ** 2
 
     def my_print(self):
         """Prints the square using '#' character."""
-
         if self.__size == 0:
             print()
         else:
