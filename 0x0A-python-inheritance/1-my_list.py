8efe6a8f126209bd990c3a35e4f3d0d0ebeeb0a6
faith221 <edithbandao14@gmail.com>
2023-10-09 09:43:29 -0700

    Python - Inheritance

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -4,11 +4,6 @@
 
 class MyList(list):
     """Implements MyList that prints a sorted list."""
-    def append(self, value):
-        """Overrides the append method to sort the list after each append."""
-        super().append(value)
-        self.sort()
-
     def print_sorted(self):
         """Prints the sorted list."""
-        print(self)
+        print(sorted(self))
