36b9cda4380422c371ad65f066a626f8d63a42bc
faith221 <edithbandao14@gmail.com>
2023-10-10 10:24:39 -0700

    New adds

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,14 +1,15 @@
 #!/usr/bin/python3
 """script that adds all arguments to a Python list."""
 import sys
-from 5-save_to_json_file import save_to_json_file
-from 6-load_from_json_file import load_from_json_file
 
+
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 filename = "add_item_json"
 try:
-    counts = load_from_json_file(filename)
+    items = load_from_json_file(filename)
 except FileNotFoundError:
-    counts = []
-counts.extend(sys.argv[1:])
-save_to_json_file(counts, filename)
+    items = []
+items.extend(sys.argv[1:])
+save_to_json_file(items, filename)
