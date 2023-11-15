ece7bbf448528d6b27655e0e43f7bb0d87560ed5
faith2221 <edithbanda014@gmail.com>
2023-11-15 09:22:23 -0800

    Correctins

--- 0x0E-SQL_more_queries/102-rating_shows.sql
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -1,7 +1,7 @@
 -- Script that lists all shows from hbtn_0d_tvshows_rate by their rating
 
-SELECT tv_genres.name, SUM(tv_show_ratings.rate) AS rating
-FROM tv_genres
+SELECT tv_shows.title, SUM(tv_show_ratings.rate) AS rating
+FROM tv_shows
 INNER JOIN tv_show_ratings
 ON tv_shows.id = tv_show_ratings.show_id
 GROUP BY tv_shows.id
