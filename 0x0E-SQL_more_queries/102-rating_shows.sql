21ef7234f37cd55d771715b74a64fadff78b633c
faith2221 <edithbanda014@gmail.com>
2023-11-15 08:55:11 -0800

    Advanced Tasks SQL - More queries

--- 0x0E-SQL_more_queries/102-rating_shows.sql
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -4,5 +4,5 @@ SELECT tv_genres.name, SUM(tv_show_ratings.rate) AS rating
 FROM tv_genres
 INNER JOIN tv_show_ratings
 ON tv_shows.id = tv_show_ratings.show_id
-GROUP BY tv_genres.id
+GROUP BY tv_shows.id
 ORDER BY rating DESC;
