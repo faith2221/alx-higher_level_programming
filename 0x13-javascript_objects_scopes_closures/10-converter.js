4ae817947b8ff43f49a30d8911b50b08374ab9e3
faith2221 <edithbanda014@gmail.com>
2023-12-11 23:13:44 -0800

    corrections

--- 0x13-javascript_objects_scopes_closures/10-converter.js
+++ 0x13-javascript_objects_scopes_closures/10-converter.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 
 exports.converter = function (base) {
-  function myConverter (num) {
-    return num.toString(base);
+  function myConverter (n) {
+    return n.toString(base);
   }
 
   return myConverter;
