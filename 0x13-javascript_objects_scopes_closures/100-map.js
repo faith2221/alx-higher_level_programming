7af8eccec36b4f9e1d3279c64c1c35cfdd4acd0b
faith2221 <edithbanda014@gmail.com>
2023-12-12 02:05:21 -0800

    Corrections to advanced tasks

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 const list = require('./100-data.js').list;
 
-const newLIST = list.map((value, index) => value * index);
+const newList = list.map((value, index) => value * index);
 console.log(list);
 console.log(newList);
